628f13b6bbbed4153f1b6dd668e9dc60f677e2d6
Kelechi Nwauwa <nwauwa.kelechi.e@gmail.com>
2021-03-28 12:55:59 +0100

    Updates

---
+++ precache-manifest.44d8dc62eff02a37d5b8e28f1b1701cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70074353ea1724c32861025ca0ae959",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "47c0ad8bc40da619ba3c",
+    "url": "/markdown-previewer/static/css/main.74d62c29.chunk.css"
+  },
+  {
+    "revision": "1c592aa0b373d1cd1da8",
+    "url": "/markdown-previewer/static/js/2.52b09be8.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/markdown-previewer/static/js/2.52b09be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c0ad8bc40da619ba3c",
+    "url": "/markdown-previewer/static/js/main.e0fd501e.chunk.js"
+  },
+  {
+    "revision": "b4b990aeecc6c23bb5f4",
+    "url": "/markdown-previewer/static/js/runtime-main.c5035dc9.js"
+  }
+]);
